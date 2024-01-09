99bee478a31ad4aecd6a0b99abda2af56c85b4a5
Thet Wai Aung <twa453354@gmail.com>
2024-01-09 22:54:19 +0630

    [Complete] SaveBlog MVC HttpClient

--- TWADotNetCore.MVC/Controllers/BlogHttpClientController.cs
+++ TWADotNetCore.MVC/Controllers/BlogHttpClientController.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
+using System;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
+using TWADotNetCore.MVC.Dtos;
 using TWADotNetCore.MVC.Models;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace TWADotNetCore.MVC.Controllers
 {
@@ -28,6 +32,39 @@ namespace TWADotNetCore.MVC.Controllers
             }
 
             return View(model);
+        }
+
+        public IActionResult CreateBlog()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveBlog(BlogDto dto)
+        {
+            BlogApiResponseModel model = new BlogApiResponseModel();
+            if (ModelState.IsValid)
+            {
+                BlogModel blog = dto.Change();
+
+                string blogJson = JsonConvert.SerializeObject(blog);
+                HttpContent httpContent = new StringContent(blogJson, Encoding.UTF8, Application.Json);
+
+                var response = await _httpClient.PostAsync("api/Blog/", httpContent);
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonStr = await response.Content.ReadAsStringAsync();
+                    model = JsonConvert.DeserializeObject<BlogApiResponseModel>(jsonStr);
+
+                    TempData["Message"] = model.Message;
+                    TempData["IsSuccess"] = model.IsSuccess;
+                    return Redirect("Index");
+                }
+            }
+
+            TempData["Message"] = model.Message;
+            TempData["IsSuccess"] = model.IsSuccess;
+            return View("CreateBlog", dto);
         }
     }
 }
