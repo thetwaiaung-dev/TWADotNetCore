c60e145b3f1c52353246c6afaa4bddfde8957777
Thet Wai Aung <twa453354@gmail.com>
2024-01-21 02:30:40 +0630

    [Complete] text file log in Log4NetRestApi using log4Net

---
+++ TWADotNetCore.Log4NetRestApi/Program.cs
@@ -0,0 +1,60 @@
+using log4net;
+using System.Reflection;
+using log4net.Config;
+
+#region log4Net
+var log4NetRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+XmlConfigurator.Configure(log4NetRepository, new FileInfo("log4net.config"));
+#endregion
+
+ILog log = LogManager.GetLogger(typeof(Program));
+log.Info("Log4Net RestApi application started...");
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddLog4Net();
+
+
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
