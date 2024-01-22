fa0e1495ba6081c1db17588cccdd22e0e5d6990a
Thet Wai Aung <twa453354@gmail.com>
2024-01-22 21:47:52 +0630

    [Complete] DB log using nLog

--- TWADotNetCore.nLogRestApi/Program.cs
+++ TWADotNetCore.nLogRestApi/Program.cs
@@ -1,18 +1,31 @@
 
+using Microsoft.Extensions.Logging;
 using NLog;
+using NLog.Web;
 
-//var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
-LogManager.Setup().LoadConfiguration(builder =>
-{
-    builder.ForLogger().FilterMinLevel(NLog.LogLevel.Debug).WriteToFile(fileName: "D:/TWADotNetCoreNlog/nlog_${shortdate}.txt");
-});
+var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+//LogManager.Setup().LoadConfiguration(builder =>
+//{
+//    builder.ForLogger()
+//            .FilterMinLevel(NLog.LogLevel.Debug)
+//            .WriteToFile(fileName: "D:/TWADotNetCoreNlog/nlog_${shortdate}.txt");
+//});
 
-var logger = LogManager.GetCurrentClassLogger();
+//var logger = LogManager.GetCurrentClassLogger();
+
+//var databaseTarget = new d
+//databaseTarget.CommandText = "insert into dbo.Log (Message) values (@Message)";
+//databaseTarget.CommandType = System.Data.CommandType.Text;
+//databaseTarget.ConnectionString = @"server=.\SQLEXPRESS;database=CiPetPet;integrated security=true;";
+//databaseTarget.DBProvider = "sqlserver";
+//databaseTarget.Parameters.Add(new DatabaseParameterInfo("@Message", "${message}"));
 try
 {
     logger.Info("Application started...");
 
     var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseNLog();
 
     // Add services to the container.
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -21,13 +34,13 @@ try
 
     var app = builder.Build();
 
+
     // Configure the HTTP request pipeline.
     if (app.Environment.IsDevelopment())
     {
         app.UseSwagger();
         app.UseSwaggerUI();
     }
-
     app.UseHttpsRedirection();
 
     var summaries = new[]
